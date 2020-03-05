2acf72102816b889da600bed582efe0101d5d810
xprilion <xprilion@gmail.com>
2020-03-06 01:47:13 +0530

    update

--- search/all_4.js
+++ search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['handle',['handle',['../d7/d20/classmake__recommendations_1_1_command.html#a53072c1075bfa2b6feb4de41d3373e9e',1,'make_recommendations::Command']]]
+  ['handle',['handle',['../de/df7/classcrawler__driver_1_1_command.html#a53072c1075bfa2b6feb4de41d3373e9e',1,'crawler_driver.Command.handle()'],['../d7/d20/classmake__recommendations_1_1_command.html#a53072c1075bfa2b6feb4de41d3373e9e',1,'make_recommendations.Command.handle()']]]
 ];
